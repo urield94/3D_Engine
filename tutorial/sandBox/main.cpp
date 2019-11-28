bc1d4a14401d68168af2e42cabc7a1128f4f215d
rochberger <drochberger@gmail.com>
2019-11-28 20:35:39 +0200

    - CHANGED: getting path from user TO getting path from configuration.txt

--- tutorial/sandBox/main.cpp
+++ tutorial/sandBox/main.cpp
@@ -1,26 +1,38 @@
-
 #include "igl/opengl/glfw/renderer.h"
 #include "tutorial/sandBox/inputManager.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char *argv[])
-{
-  if(argc < 2){
-    std::cout << "Please specify path to meshe files (as many as you wish).\nUsage - sandBox.exe full_path_to_mesh_1 full_path_to_mesh_2 .." << std::endl;
-    return  0;
-  }
+using namespace std;
 
-  Display *disp = new Display(1000, 800, "Wellcome");
-  Renderer renderer;
-  igl::opengl::glfw::Viewer viewer;
+igl::opengl::glfw::Viewer load_meshes_from_conf() {
+    igl::opengl::glfw::Viewer viewer;;
+    fstream newfile;
+    newfile.open("configuration.txt", ios::in);
+    if (newfile.is_open()) {
+        string line;
+        while (getline(newfile, line)) {
+            viewer.load_mesh_from_file(line);
+        }
+        newfile.close();
+    }
+    return viewer;
+}
 
-  for(int i = 1; i < argc; i++){
-    viewer.load_mesh_from_file(argv[i]);
-  }
+int main(int argc, char *argv[]) {
 
-  Init(*disp);
-  renderer.init(&viewer);
-  disp->SetRenderer(&renderer);
-  disp->launch_rendering(true);
-  
-  delete disp;
+    std::ifstream input("configuration.txt");
+
+    Display *disp = new Display(1000, 800, "Wellcome");
+    Renderer renderer;
+
+    igl::opengl::glfw::Viewer viewer = load_meshes_from_conf();
+
+    Init(*disp);
+    renderer.init(&viewer);
+    disp->SetRenderer(&renderer);
+    disp->launch_rendering(true);
+
+    delete disp;
 }
