a1d3de20f24026d7443216c5220a9fd4109bf488
rochberger <drochberger@gmail.com>
2019-12-08 18:45:33 +0200

    * Assignment 2: Tasks 10 -> my_collapse function was added. Has segmentation fault after some iterations.

--- tutorial/sandBox/ViewerInh.h
+++ tutorial/sandBox/ViewerInh.h
@@ -8,33 +8,51 @@
 #include <queue>
 #include <iostream>
 
+typedef std::set<std::pair<double,int> > PriorityQueue;
+typedef struct {
+    int num_collapsed;
+    Eigen::MatrixXi E;
+    Eigen::MatrixXi EF;
+    Eigen::MatrixXi EI;
+    Eigen::VectorXi EMAP;
+    Eigen::MatrixXd C;
+    PriorityQueue Q;
+    std::vector<PriorityQueue::iterator > Qit;
+    std::vector<Eigen::Matrix4d> EQ; //Error Quadrics vector
+} edges;
 
 class ViewerInh: public igl::opengl::glfw::Viewer {
     public:
-//        typedef std::set<std::pair<double,int> > PriorityQueue;
-        typedef std::pair<double, int> pi;
-        struct edges{
-            int num_collapsed;
-            Eigen::MatrixXi E;
-            Eigen::MatrixXi EF;
-            Eigen::MatrixXi EI;
-            Eigen::VectorXi EMAP;
-            std::priority_queue <pi, std::vector<pi>, std::greater<pi>> Q;
-            std::vector<Eigen::Vector3d> C;
-        };
-        std::vector<edges> data_edges;
-        void my_collapse(  const int e,
-                           const Eigen::RowVectorXd & p,
-                           Eigen::MatrixXd & V,
-                           Eigen::MatrixXi & F,
-                           Eigen::MatrixXi & E,
-                           Eigen::VectorXi & EMAP,
-                           Eigen::MatrixXi & EF,
-                           Eigen::MatrixXi & EI);
-        edges reset(igl::opengl::ViewerData obj);
+
+        edges *reset(igl::opengl::ViewerData obj, edges* ds);
         void simplify(int num_to_collapse);
         void init_ds_for_data();
         void simplification();
+        ~ViewerInh() {
+            for (auto &ds : data_edges) {
+                delete(ds);
+            }
+        }
+        std::vector<edges*> data_edges;
+private:
+
+    static std::vector<Eigen::Matrix4d> get_Q(Eigen::MatrixXd &V, Eigen::MatrixXi &F, Eigen::MatrixXd &F_normals);
+    static  void calc_edges_cost( const int e,
+                                     const Eigen::MatrixXd & V,
+                                     const Eigen::MatrixXi & F,
+                                     const Eigen::MatrixXi & E,
+                                     const Eigen::VectorXi & EMAP,
+                                     const Eigen::MatrixXi & EF,
+                                     const Eigen::MatrixXi & EI,
+                                     double & cost,
+                                     Eigen::RowVectorXd & p,
+                                     std::vector<Eigen::Matrix4d> &Q);
+    static bool my_collapse(Eigen::MatrixXd & V,
+                            Eigen::MatrixXi & F,
+                            Eigen::MatrixXd &F_normals,
+                            edges *ds);
+
+
 };
 
 
