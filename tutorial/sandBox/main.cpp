cc4fccba1ceac56bd9f4d8a1641a1241d56bea57
rochberger <drochberger@gmail.com>
2020-02-12 22:19:58 +0200

    Set\Get Player from SQL-DB

--- tutorial/sandBox/main.cpp
+++ tutorial/sandBox/main.cpp
@@ -11,8 +11,19 @@
 #include <sqlite3.h>
 #include <stdio.h>
 
+#define SQL_ERROR(err_msg){\
+            fprintf(stderr, "Something went terribly wrong- SQL error: %s\n", err_msg);\
+            sqlite3_free(err_msg);\
+            return 0;\
+}
+
 using namespace std;
 
+string player_name;
+string player_id_str;
+int player_id;
+int player_score;
+int player_level;
 
 igl::opengl::glfw::Viewer load_meshes_from_conf(Renderer *rndr) {
     igl::opengl::glfw::Viewer viewer;;
@@ -48,40 +59,122 @@ igl::opengl::glfw::Viewer load_meshes_from_conf(Renderer *rndr) {
     return viewer;
 }
 
-static int callback(void *NotUsed, int num_of_values, char **values, char **keys) {
-    int i;
-    for(i = 0; i<num_of_values; i++) {
-        printf("%s = %s\n", keys[i], values[i] ? values[i] : "NULL");
-    }
-    printf("\n");
+static int do_nothing_callback(void *none, int num_of_values, char **values, char **keys) {
+    return 1;
+}
+
+static int set_player_id(void *none, int num_of_values, char **values, char **keys) {
+    //Get last-id in "games" table as char*, convert it to string and then to int.
+    stringstream last_id_str;
+    unsigned int last_id;
+
+    last_id_str << values[0];
+    last_id_str >> last_id;
+
+    // Set_player_id
+    player_id = last_id + 1;
+    player_id_str = std::to_string(player_id);
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+static int set_existing_player(void *notUsed, int num_of_values, char **values, char **keys) {
+    for(int i = 0; i<num_of_values; i++) {
+        if(!std::strcmp(keys[i],"username")) {
+            player_name = values[i];
+        }else{
+            stringstream val_str;
+            unsigned int val_number;
+            val_str << values[i];
+            val_str >> val_number;
+            if (!std::strcmp(keys[i], "id")) {
+                player_id = val_number;
+            } else if (!std::strcmp(keys[i], "score")) {
+                player_score = val_number;
+            } else if (!std::strcmp(keys[i], "level")) {
+                player_level = val_number;
+            }
+        }
+    }
+    return 1;
+}
+
+int add_new_player(sqlite3 *db, string name) {
+    player_name = name;
+
+    char *zErrMsg = 0;
+    int rc;
+
+    char max_id_query[] = "SELECT MAX(id) FROM games";
+    rc = sqlite3_exec(db, max_id_query, set_player_id, 0, &zErrMsg);
+    if (rc == SQLITE_OK) { // Player_id loaded successfully!
+        string insert_query = "INSERT INTO games (id,username,score,level) VALUES (" + player_id_str + ", '" + player_name + "', 0, 0);";
+        rc = sqlite3_exec(db, insert_query.c_str(), do_nothing_callback, 0, &zErrMsg);
+        if (rc != SQLITE_OK) {
+            SQL_ERROR(zErrMsg); // Player was not added to the DB.
+        }
+    } else {
+        SQL_ERROR(zErrMsg); // Player_id wasn't loaded.
+    }
+    return 1;
+}
+
+int get_existing_player(sqlite3 *db, string name) {
+    char *zErrMsg = 0;
+    int rc = 0;
+    string get_user_details = "SELECT * FROM games WHERE username = '" + name + "'";
+    rc = sqlite3_exec(db, get_user_details.c_str(), set_existing_player, 0, &zErrMsg);
+    if (!rc) {
+        return add_new_player(db, name);
+    }else{
+        return 1;
+    }
+}
+
+
+
+
+int set_db() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
-    const char* data = "Callback function called";
-
     rc = sqlite3_open("game.db", &db);
 
-    if( rc ) {
+    if (rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        return(0);
+        return 0;
     } else {
-        char * sql = "";
-        rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-        if( rc != SQLITE_OK ){
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
+        string create_games_table = "CREATE TABLE IF NOT EXISTS games("  \
+                                      "id INTEGER PRIMARY KEY     NOT NULL," \
+                                      "username           TEXT    NOT NULL," \
+                                      "score              INTEGER NOT NULL," \
+                                      "level              INTEGER NOT NULL);";
+
+        rc = sqlite3_exec(db, create_games_table.c_str(), do_nothing_callback, 0, &zErrMsg);
+
+        if (rc == SQLITE_OK) {
+            string ans;
+            cout<< "Enter Username - ";
+            getline(cin, ans);
+            int success =  get_existing_player(db, ans);
+            if (!success) {
+                return 0;
+            }
         } else {
-//            fprintf(stdout, "Operation done successfully\n");
+            SQL_ERROR(zErrMsg); // Table was not created.
         }
     }
-
     sqlite3_close(db);
 
-    srand (static_cast <unsigned> (time(0)));
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+
+    int success = set_db();
+    if(!success)
+        return 1;
+
+    srand(static_cast <unsigned> (time(0)));
     std::ifstream input("configuration.txt");
 
     Display *disp = new Display(1700, 850, "Wellcome");
@@ -97,7 +190,9 @@ int main(int argc, char *argv[]) {
     renderer.append_core(Eigen::Vector4f(850, 0, 850, 850));
 
     disp->SetRenderer(&renderer);
+
     disp->launch_rendering(true);
 
     delete disp;
 }
+
